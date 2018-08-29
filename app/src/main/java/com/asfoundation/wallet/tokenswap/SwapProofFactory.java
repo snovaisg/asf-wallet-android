ab3a0d34c3aeba61c13a11f3b8a4fedf901ccdda
Simao Novais <>
2018-08-29 15:07:21 +0100

    GasLimit adjusted to Kyber swaps

--- app/src/main/java/com/asfoundation/wallet/tokenswap/SwapProofFactory.java
+++ app/src/main/java/com/asfoundation/wallet/tokenswap/SwapProofFactory.java
@@ -11,7 +11,7 @@ public class SwapProofFactory {
         "0x818E6FECD516Ecc3849DAf6845e3EC868087B755"; // hack KyberNetworkProxy Ropsten address by default;
     BigDecimal gasPrice = Convert.toWei("50",
         Convert.Unit.GWEI); // hack hardcoded for now. Returns 50 Gwei ( current accepted gasPrice of Ropsten to swap )
-    BigDecimal gasLimit = BigDecimal.valueOf(250000);
+    BigDecimal gasLimit = BigDecimal.valueOf(450000);
     SwapProof swapProof = new SwapProof();
     swapProof.setChainId(chainId);
     swapProof.setGasPrice(gasPrice);
